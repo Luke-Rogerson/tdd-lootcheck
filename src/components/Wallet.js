12e57c57a0d0fe6996f489f66ebf1463e58bc52b
Luke Rogerson <lukerogerson@aol.com>
2019-01-20 20:06:56 +0000

    Set local wallet balance in statge

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  state = {
+    balance: undefined
+  };
+
+  updateBalance = event => {
+    this.setState({ balance: parseInt(event.target.value, 10) });
+  };
   render() {
     return (
       <div>
         <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+        <input className='input-wallet' onChange={this.updateBalance} />
       </div>
     );
   }
--- src/components/wallet.test.js
+++ src/components/wallet.test.js
@@ -11,4 +11,20 @@ describe('Wallet', () => {
   it('displays the balance from props', () => {
     expect(wrapper.find('.balance').text()).toEqual('Wallet balance: 20');
   });
+
+  it('creates an input to deposit into or withdraw from the balance', () => {
+    expect(wrapper.find('.input-wallet').exists()).toBe(true);
+  });
+
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+    beforeEach(() => {
+      wrapper
+        .find('.input-wallet')
+        .simulate('change', { target: { value: userBalance } });
+    });
+    it('updates the local wallet balance in `state` and it converts it to a number', () => {
+      expect(wrapper.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+  });
 });
